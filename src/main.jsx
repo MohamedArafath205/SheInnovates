b9dfe9a9cf729225e5755a25b9ccd244b6a2cdf0
MohamedArafath205 <mohamedarafath205@gmail.com>
2024-03-07 17:50:29 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{backgroundColor: 'black'}}>
+      <h1 style={{color: 'white'}}>Hello, world!</h1>
+    </div>
+  );
+}
+
+export default App;
